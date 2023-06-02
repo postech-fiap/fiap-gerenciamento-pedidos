bc61db7b93920cccc2dbd17ac50ee840618f8b90
Everton91Almeida <everton.almeida@mercadolivre.com>
2023-06-01 22:45:49 -0300

    initial commit

---
+++ src/main/kotlin/br/com/fiap/gerenciamentopedidos/GerenciamentoPedidosApplication.kt
@@ -0,0 +1,11 @@
+package br.com.fiap.gerenciamentopedidos
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GerenciamentoPedidosApplication
+
+fun main(args: Array<String>) {
+	runApplication<GerenciamentoPedidosApplication>(*args)
+}
