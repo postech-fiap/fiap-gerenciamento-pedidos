9844978b1b33d2f04ff4827e16578efa314a34a5
Everton Almeida <everton.almeida@mercadolivre.com>
2023-06-13 23:11:52 -0300

    Feature/ajustes estrutura projeto (#22)
    * Classe Application renomeada
    
    * Alteração dockerfile
    
    * Alteração dockerfile

--- app/api/src/main/kotlin/br/com/fiap/gerenciamentopedidos/api/Application.kt
+++ app/api/src/main/kotlin/br/com/fiap/gerenciamentopedidos/api/Application.kt
@@ -4,8 +4,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class GerenciamentoPedidosApplication
+class Application
 
 fun main(args: Array<String>) {
-	runApplication<GerenciamentoPedidosApplication>(*args)
+	runApplication<Application>(*args)
 }
