ea83d4dcdee335d6f1d6a67b84e36a88349ec836
Gabriel Henrique Leal Garcia <gabrielgarciasp@gmail.com>
2023-06-19 22:55:13 -0300

    Improvements after meet

--- scripts/migration/001-first-structure.sql
+++ scripts/migration/001-first-structure.sql
@@ -1,59 +1,61 @@
 use gerenciamento_pedidos_db;
 
 create table if not exists cliente (
-    id bigint,
+    id bigint auto_increment,
     cpf char(11) not null,
     email varchar(100) not null,
     nome varchar(100) not null,
-    constraint pk_cliente primary key (id)
+    constraint pk_cliente primary key (id),
+    constraint uc_cliente_cpf unique (cpf)
 );
 
-create table if not exists item (
-    id bigint,
-    valor decimal(15,2) not null,
-    nome varchar(50) not null,
-    categoria smallint not null,
+create table if not exists produto (
+    id bigint auto_increment,
+    valor decimal(10,2) not null,
+    nome varchar(100) not null,
+    categoria enum('lanche', 'acompanhamento', 'bebida', 'sobremesa') not null,
     disponivel bit(1) not null,
     excluido bit(1) not null,
-    descricao varchar(250),
-    constraint pk_item primary key (id)
+    descricao varchar(1000),
+    tempo_preparo int not null,
+    constraint pk_produto primary key (id)
 );
 
-create table if not exists item_imagem (
-    id bigint,
+create table if not exists produto_imagem (
+    produto_id bigint not null,
     caminho varchar(100) not null,
-    item_id bigint not null,
-    constraint pk_item_imagem primary key (id),
-    constraint fk_item_imagem_item foreign key (item_id) references item (id) on delete restrict on update restrict
+    constraint pk_produto_imagem primary key (produto_id),
+    constraint fk_produto_imagem_produto foreign key (produto_id) references produto (id) on delete restrict on update restrict
 );
 
 create table if not exists pedido (
-    id bigint,
-    data_hora timestamp not null,
-    status varchar(20) not null,
+    id bigint auto_increment,
+    data_hora timestamp not null default now(),
+    status enum('pendente', 'em_preparacao', 'finalizado') not null,
     cliente_id bigint,
-    tempo_preparo smallint not null,
-    numero varchar(10) not null,
+    tempo_espera int not null,
+    numero char(4) not null,
     constraint pk_pedido primary key (id),
     constraint fk_pedido_cliente foreign key (cliente_id) references cliente (id) on delete restrict on update restrict
 );
 
-create table if not exists pedido_item (
+create table if not exists pedido_produto (
     pedido_id bigint,
-    item_id bigint,
-    valor_pago decimal(15,2) not null,
-    quantidade smallint not null,
-    comentario varchar(100),
-    constraint pk_pedido_item primary key (pedido_id, item_id),
-    constraint fk_pedido_item_pedido foreign key (pedido_id) references pedido (id) on delete restrict on update restrict,
-    constraint fk_pedido_item_item foreign key (item_id) references item (id) on delete restrict on update restrict
+    produto_id bigint,
+    valor_pago decimal(10,2) not null,
+    quantidade int not null,
+    comentario varchar(1000),
+    constraint pk_pedido_produto primary key (pedido_id, produto_id),
+    constraint fk_pedido_produto_pedido foreign key (pedido_id) references pedido (id) on delete restrict on update restrict,
+    constraint fk_pedido_produto_produto foreign key (produto_id) references produto (id) on delete restrict on update restrict
 );
 
 create table if not exists pagamento (
-    id bigint,
-    data_hora timestamp not null,
-    status smallint not null,
     pedido_id bigint not null,
-    constraint pk_pagamento primary key (id),
+    data_hora timestamp not null default now(),
+    status enum('pendente', 'aprovado', 'rejeitado') not null,
+    constraint pk_pagamento primary key (pedido_id),
     constraint fk_pagamento_pedido foreign key (pedido_id) references pedido (id) on delete restrict on update restrict
 );
+
+# 9.999.999,99
