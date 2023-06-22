47213b80dc7b15b27ac098cade9b81609f28faa0
Melissa Teodoro <melissa.miranda@mercadolivre.com>
2023-06-22 19:46:36 -0300

    Feature/add pedido context to package (#23)
    * refact: add context package pedido
    
    * refact: add context package pedido on application

---
+++ app/domain/src/main/kotlin/br/com/fiap/gerenciamentopedidos/domain/pedido/interfaces/services/PedidoService.kt
@@ -0,0 +1,4 @@
+package br.com.fiap.gerenciamentopedidos.domain.pedido.interfaces.services
+
+interface PedidoService {
+}
--- app/domain/src/main/kotlin/br/com/fiap/gerenciamentopedidos/domain/services/PedidoServiceImpl.kt
+++
@@ -1,6 +0,0 @@
-package br.com.fiap.gerenciamentopedidos.domain.services
-
-import br.com.fiap.gerenciamentopedidos.domain.interfaces.services.PedidoService
-
-class PedidoServiceImpl : PedidoService {
-}
