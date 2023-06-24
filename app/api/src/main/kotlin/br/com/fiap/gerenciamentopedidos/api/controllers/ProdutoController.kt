9945bc0595c3f2b881d0d4dc06feb0adb402ea0f
IcaroJurado <icaro.jurado@mercadolivre.com>
2023-06-24 19:55:38 -0300

    creating new endpoint to list all categories from enum

--- app/api/src/main/kotlin/br/com/fiap/gerenciamentopedidos/api/controllers/ProdutoController.kt
+++ app/api/src/main/kotlin/br/com/fiap/gerenciamentopedidos/api/controllers/ProdutoController.kt
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-class ItemController {
+class ProdutoController {
 
     @GetMapping("/categorias")
     fun listarCategorias(): List<Categorias> {
