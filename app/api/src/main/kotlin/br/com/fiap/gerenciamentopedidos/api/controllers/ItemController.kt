880c3c6b3a242cb49a4aeb400a8ae44c425340d2
IcaroJurado <icaro.jurado@mercadolivre.com>
2023-06-24 19:36:51 -0300

    creating new endpoint to list all categories from enum

---
+++ app/api/src/main/kotlin/br/com/fiap/gerenciamentopedidos/api/controllers/ItemController.kt
@@ -0,0 +1,14 @@
+package br.com.fiap.gerenciamentopedidos.api.controllers
+
+import br.com.fiap.gerenciamentopedidos.domain.pedido.models.Categorias
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class ItemController {
+
+    @GetMapping("/categorias")
+    fun listarCategorias(): List<Categorias> {
+        return Categorias.values().toList()
+    }
+}
---
+++ app/domain/src/main/kotlin/br/com/fiap/gerenciamentopedidos/domain/pedido/models/Categorias.kt
@@ -0,0 +1,8 @@
+package br.com.fiap.gerenciamentopedidos.domain.pedido.models
+
+enum class Categorias {
+    BEBIDA,
+    ACOMPANHAMENTO,
+    LANCHE,
+    SOBREMESA
+}
