bbc841912be346c9b46d611608dcda3c7f5d1010
Everton91Almeida <everton.almeida@mercadolivre.com>
2023-06-27 10:46:23 -0300

    add @value on database config

--- app/api/src/main/kotlin/br/com/fiap/gerenciamentopedidos/api/config/DatabaseConfig.kt
+++ app/api/src/main/kotlin/br/com/fiap/gerenciamentopedidos/api/config/DatabaseConfig.kt
@@ -2,6 +2,7 @@ package br.com.fiap.gerenciamentopedidos.api.config
 
 import com.zaxxer.hikari.HikariConfig
 import com.zaxxer.hikari.HikariDataSource
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.autoconfigure.domain.EntityScan
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
@@ -9,17 +10,26 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories
 import javax.sql.DataSource
 
 @Configuration
-@EnableJpaRepositories(basePackages=["br.com.fiap.gerenciamentopedidos.infrastructure"])
-@EntityScan(basePackages=["br.com.fiap.gerenciamentopedidos.infrastructure"])
-class DatabaseConfig {
+@EnableJpaRepositories(basePackages = ["br.com.fiap.gerenciamentopedidos.infrastructure"])
+@EntityScan(basePackages = ["br.com.fiap.gerenciamentopedidos.infrastructure"])
+class DatabaseConfig(
+    @Value("\${spring.datasource.driver-class-name}")
+    private val driverClassName: String,
+    @Value("\${spring.datasource.url}")
+    private val jdbcUrl: String,
+    @Value("\${spring.datasource.username}")
+    private val username: String,
+    @Value("\${spring.datasource.password}")
+    private val password: String
+) {
     @Bean
     fun dataSource(): DataSource {
         val config = HikariConfig()
 
-        config.driverClassName = "com.mysql.cj.jdbc.Driver"
-        config.jdbcUrl = "jdbc:mysql://localhost:3306/gerenciamento_pedidos_db"
-        config.username = "root"
-        config.password = "123456"
+        config.driverClassName = driverClassName
+        config.jdbcUrl = jdbcUrl
+        config.username = username
+        config.password = password
         config.isAutoCommit = true
 
         return HikariDataSource(config)
