9ddaa6c528eb33cd2f82b3984376ee498190e43c
Melissa Teodoro <melissa.miranda@mercadolivre.com>
2023-06-28 20:31:25 -0300

    Feature/improvements client registration (#28)
    * refact: add cpf to client path
    
    * feat: adding Swagger on client endpoint

--- app/api/src/main/kotlin/br/com/fiap/gerenciamentopedidos/api/controllers/ClienteController.kt
+++ app/api/src/main/kotlin/br/com/fiap/gerenciamentopedidos/api/controllers/ClienteController.kt
@@ -4,6 +4,12 @@ import br.com.fiap.gerenciamentopedidos.api.request.ClienteRequest
 import br.com.fiap.gerenciamentopedidos.api.response.ClienteResponse
 import br.com.fiap.gerenciamentopedidos.application.cadastro.interfaces.BuscarClientePorCpfUseCase
 import br.com.fiap.gerenciamentopedidos.application.cadastro.interfaces.CadastrarClienteUseCase
+import br.com.fiap.gerenciamentopedidos.domain.cadastro.exceptions.BaseDeDadosException
+import br.com.fiap.gerenciamentopedidos.domain.cadastro.exceptions.RecursoJaExisteException
+import br.com.fiap.gerenciamentopedidos.domain.cadastro.exceptions.RecursoNaoEncontradoException
+import io.swagger.annotations.ApiOperation
+import io.swagger.annotations.ApiResponse
+import io.swagger.annotations.ApiResponses
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
@@ -18,9 +24,21 @@ import org.springframework.web.bind.annotation.RestController
 class ClienteController(
     val cadastrarClienteUseCase: CadastrarClienteUseCase,
     val buscarClientePorCpfUseCase: BuscarClientePorCpfUseCase
-    ) {
+) {
 
-    @PostMapping
+    companion object {
+        const val CPF_URI = "/cpf"
+    }
+
+    @ApiOperation("Responsável por cadastrar um cliente")
+    @ApiResponses(
+        ApiResponse(code = 201, message = "Created", response = ClienteResponse::class),
+        ApiResponse(code = 409, message = "Conflict", response = RecursoJaExisteException::class),
+        ApiResponse(code = 400, message = "Bad Request", response = IllegalArgumentException::class),
+        ApiResponse(code = 500, message = "Internal Server Error", response = BaseDeDadosException::class),
+        ApiResponse(code = 500, message = "Internal Server Error", response = Exception::class)
+    )
+    @PostMapping(CPF_URI)
     fun cadastrarCliente(@RequestBody clienteRequest: ClienteRequest): ResponseEntity<ClienteResponse> {
         val cliente = cadastrarClienteUseCase.executar(clienteRequest.toDomain())
 
@@ -28,7 +46,14 @@ class ClienteController(
             .body(ClienteResponse.fromDomain(cliente))
     }
 
-    @GetMapping("/{cpf}")
+    @ApiOperation("Responsável por buscar um cliente")
+    @ApiResponses(
+        ApiResponse(code = 200, message = "Ok", response = ClienteResponse::class),
+        ApiResponse(code = 404, message = "Not Found", response = RecursoNaoEncontradoException::class),
+        ApiResponse(code = 500, message = "Internal Server Error", response = BaseDeDadosException::class),
+        ApiResponse(code = 500, message = "Internal Server Error", response = Exception::class)
+    )
+    @GetMapping("$CPF_URI/{cpf}")
     fun buscarClientePorCpf(@PathVariable cpf: String): ResponseEntity<ClienteResponse> {
         val cliente = buscarClientePorCpfUseCase.executar(cpf)
 
