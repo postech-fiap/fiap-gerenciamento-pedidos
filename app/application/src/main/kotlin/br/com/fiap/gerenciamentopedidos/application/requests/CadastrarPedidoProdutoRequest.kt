bbf08cecf6df0c3e1fbb1c797e99fa800bdab672
GabrielChaves <ghchaves@msn.com>
2023-07-06 12:27:06 -0300

    validations

--- app/application/src/main/kotlin/br/com/fiap/gerenciamentopedidos/application/requests/CadastrarPedidoProdutoRequest.kt
+++ app/application/src/main/kotlin/br/com/fiap/gerenciamentopedidos/application/requests/CadastrarPedidoProdutoRequest.kt
@@ -8,4 +8,9 @@ data class CadastrarPedidoProdutoRequest(
     val produtoId: Long,
     val quantidade: Long,
     val comentario: String? = null
-)
+){
+    init {
+        require(produtoId > 0) { "O id do produto deve ser informado" }
+        require(quantidade > 0) { "A quantidade deve ser maior que zero" }
+    }
+}
