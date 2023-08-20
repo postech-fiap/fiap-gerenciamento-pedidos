01862360abce90887f0accfa740eb25cc175e0fa
Everton91Almeida <everton.almeida@mercadolivre.com>
2023-08-19 23:30:54 -0300

    change params and returns of usecases

--- app/application/src/main/kotlin/br/com/fiap/gerenciamentopedidos/application/usecases/produto/AlterarDisponibilidadeProdutoUseCaseImpl.kt
+++ app/application/src/main/kotlin/br/com/fiap/gerenciamentopedidos/application/usecases/produto/AlterarDisponibilidadeProdutoUseCaseImpl.kt
@@ -1,16 +1,16 @@
 package br.com.fiap.gerenciamentopedidos.application.usecases.produto
 
 import br.com.fiap.gerenciamentopedidos.application.interfaces.produto.AlterarDisponibilidadeProdutoUseCase
-import br.com.fiap.gerenciamentopedidos.application.responses.ProdutoResponse
 import br.com.fiap.gerenciamentopedidos.domain.exceptions.RecursoNaoEncontradoException
 import br.com.fiap.gerenciamentopedidos.domain.interfaces.ProdutoRepository
+import br.com.fiap.gerenciamentopedidos.domain.models.Produto
 
 class AlterarDisponibilidadeProdutoUseCaseImpl(private val produtoRepository: ProdutoRepository) :
     AlterarDisponibilidadeProdutoUseCase {
-    override fun executar(id: Long, disponivel: Boolean): ProdutoResponse {
-        val produto =
-            produtoRepository.get(id).orElseThrow { RecursoNaoEncontradoException("Produto não encontrado") }
+    override fun executar(id: Long, disponivel: Boolean): Produto {
+        val produto = produtoRepository.get(id)
+            .orElseThrow { RecursoNaoEncontradoException("Produto não encontrado") }
         produto.alterarDisponibilidade(disponivel)
-        return ProdutoResponse(produtoRepository.update(produto))
+        return produtoRepository.update(produto)
     }
 }
