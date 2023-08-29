4099eb4cf1c87fb76ec41a85d7e43120e5b0c8a0
Mel <melmelissa1936@gmail.com>
2023-08-28 23:20:45 -0300

    refact: remove unnecessary class

--- app/domain/src/main/kotlin/br/com/fiap/gerenciamentopedidos/domain/interfaces/gateways/BuscarPedidoPorIdGateway.kt
+++
@@ -1,7 +0,0 @@
-package br.com.fiap.gerenciamentopedidos.domain.interfaces.gateways
-
-import br.com.fiap.gerenciamentopedidos.domain.models.mercadoPago.DetalhePagamento
-
-interface BuscarPedidoPorIdGateway {
-    fun executar(id: String): DetalhePagamento
-}
