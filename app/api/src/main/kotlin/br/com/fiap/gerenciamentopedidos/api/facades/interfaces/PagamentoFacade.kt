de13c7cdda0c99de8776842ebeb0ae69b6d73fae
Mel <melmelissa1936@gmail.com>
2023-08-28 23:20:06 -0300

    feat: add update status pagamento and pedido on FinalizarPagamentoUseCase

--- app/api/src/main/kotlin/br/com/fiap/gerenciamentopedidos/api/facades/interfaces/PagamentoFacade.kt
+++ app/api/src/main/kotlin/br/com/fiap/gerenciamentopedidos/api/facades/interfaces/PagamentoFacade.kt
@@ -1,8 +1,9 @@
 package br.com.fiap.gerenciamentopedidos.api.facades.interfaces
 
 import br.com.fiap.gerenciamentopedidos.api.requests.PagamentoCriadoRequest
+import br.com.fiap.gerenciamentopedidos.api.responses.DetalhePagamentoResponse
 
 interface PagamentoFacade {
 
-    fun finalizarPagamento(request: PagamentoCriadoRequest)
+    fun finalizarPagamento(request: PagamentoCriadoRequest): DetalhePagamentoResponse
 }
