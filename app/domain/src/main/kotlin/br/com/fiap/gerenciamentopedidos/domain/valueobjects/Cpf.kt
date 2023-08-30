a23e1fd6740fdcab768718a8c40bc3946a6bb7d4
IcaroJurado <icaro.jurado@mercadolivre.com>
2023-08-30 17:24:46 -0300

    adding cpf validation

--- app/domain/src/main/kotlin/br/com/fiap/gerenciamentopedidos/domain/valueobjects/Cpf.kt
+++ app/domain/src/main/kotlin/br/com/fiap/gerenciamentopedidos/domain/valueobjects/Cpf.kt
@@ -5,6 +5,31 @@ data class Cpf(val numero: String) {
     init {
         require(numero.isNotBlank()) { "CPF não pode ser vazio" }
         require(numero.matches(Regex("(\\d{3}\\.\\d{3}\\.\\d{3}\\-\\d{2})|\\d{11}"))) { "Formato do CPF é inválido!" }
+        require(isCPF(numero))
+    }
+
+    private fun isCPF(document: String): Boolean {
+        if (document.isEmpty()) return false
+
+        val numbers = document.filter { it.isDigit() }.map {
+            it.toString().toInt()
+        }
+
+        if (numbers.size != 11) return false
+
+        //repeticao
+        if (numbers.all { it == numbers[0] }) return false
+
+        //digito 1
+        val dv1 = ((0..8).sumOf { (it + 1) * numbers[it] }).rem(11).let {
+            if (it >= 10) 0 else it
+        }
+
+        val dv2 = ((0..8).sumOf { it * numbers[it] }.let { (it + (dv1 * 9)).rem(11) }).let {
+            if (it >= 10) 0 else it
+        }
+
+        return numbers[9] == dv1 && numbers[10] == dv2
     }
 
     fun removeMascara() = removeMascara(numero)
