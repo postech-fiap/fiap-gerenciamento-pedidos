bf59db0283f308dae184f6e688a7fff592b7ac1f
Mel <melmelissa1936@gmail.com>
2023-08-31 17:18:52 -0300

    fix: replace id pedido by numero pedido to externalReference

--- app/domain/src/main/kotlin/br/com/fiap/gerenciamentopedidos/domain/dtos/MercadoPagoOrdemDto.kt
+++ app/domain/src/main/kotlin/br/com/fiap/gerenciamentopedidos/domain/dtos/MercadoPagoOrdemDto.kt
@@ -31,7 +31,7 @@ data class MercadoPagoOrdemDto(
     companion object {
         fun fromDto(pedido: Pedido, webhookUrl: String) =
             MercadoPagoOrdemDto(
-                externalReference = pedido.id.toString(),
+                externalReference = pedido.numero.toString(),
                 totalAmount = pedido.valorTotal!!,
                 items = pedido.items.map {
                     MercadoPagoItensDto(
