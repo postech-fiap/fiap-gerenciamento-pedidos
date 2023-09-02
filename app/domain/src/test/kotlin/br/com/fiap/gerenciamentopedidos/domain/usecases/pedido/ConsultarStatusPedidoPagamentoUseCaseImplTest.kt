19b96489a19432bd5a1efedb736865b2f4749732
Everton Almeida <everton.almeida@mercadolivre.com>
2023-09-01 21:35:58 -0300

    fix tests (#107)

--- app/domain/src/test/kotlin/br/com/fiap/gerenciamentopedidos/domain/usecases/pedido/ConsultarStatusPedidoPagamentoUseCaseImplTest.kt
+++ app/domain/src/test/kotlin/br/com/fiap/gerenciamentopedidos/domain/usecases/pedido/ConsultarStatusPedidoPagamentoUseCaseImplTest.kt
@@ -48,7 +48,7 @@ class ConsultarStatusPedidoPagamentoUseCaseImplTest {
     private fun criarPedido(): Pedido {
         val pedido = Pedido("1")
         pedido.gerarQrCodePagamento(Pagamento(1, OffsetDateTime.now(), PagamentoStatus.APROVADO, "", BigDecimal(10)))
-        pedido.atribuirCliente(Cliente(1, Cpf("22233388878"), "Derick Silva", Email("dsilva@gmail.com")))
+        pedido.atribuirCliente(Cliente(1, Cpf("73139333552"), "Derick Silva", Email("dsilva@gmail.com")))
         pedido.adicionarItem(criarItem())
         return pedido
     }
