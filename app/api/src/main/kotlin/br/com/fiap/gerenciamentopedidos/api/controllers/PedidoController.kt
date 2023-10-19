f993985c62cda5f875a340d68eeb40b8848d4699
Everton91Almeida <everton.almeida@mercadolivre.com>
2023-10-19 19:52:15 -0300

    add jacoco config

--- app/api/src/main/kotlin/br/com/fiap/gerenciamentopedidos/api/controllers/PedidoController.kt
+++ app/api/src/main/kotlin/br/com/fiap/gerenciamentopedidos/api/controllers/PedidoController.kt
@@ -3,15 +3,7 @@ package br.com.fiap.gerenciamentopedidos.api.controllers
 import br.com.fiap.gerenciamentopedidos.api.adapters.interfaces.PedidoAdapter
 import br.com.fiap.gerenciamentopedidos.api.requests.AlterarStatusPedidoRequest
 import br.com.fiap.gerenciamentopedidos.api.requests.CadastrarPedidoRequest
-import br.com.fiap.gerenciamentopedidos.api.responses.PagamentoStatusResponse
 import br.com.fiap.gerenciamentopedidos.api.responses.PedidoResponse
-import br.com.fiap.gerenciamentopedidos.infrastructure.exceptions.BaseDeDadosException
-import io.swagger.v3.oas.annotations.Operation
-import io.swagger.v3.oas.annotations.media.ArraySchema
-import io.swagger.v3.oas.annotations.media.Content
-import io.swagger.v3.oas.annotations.media.Schema
-import io.swagger.v3.oas.annotations.responses.ApiResponse
-import io.swagger.v3.oas.annotations.responses.ApiResponses
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 import java.net.URI
@@ -19,90 +11,19 @@ import java.net.URI
 @RestController
 @RequestMapping("/pedidos")
 class PedidoController(private val pedidoAdapter: PedidoAdapter) {
-    @Operation(summary = "Busca pedidos por status")
-    @ApiResponses(
-        value = [ApiResponse(
-            responseCode = "200", description = "Ok",
-            content = [(Content(
-                mediaType = "application/json",
-                array = ArraySchema(schema = Schema(implementation = PedidoResponse::class))
-            )
-                    )]
-        ), ApiResponse(
-            responseCode = "500",
-            description = "Internal Server Error",
-            content = [Content(
-                mediaType = "application/json",
-                schema = Schema(implementation = BaseDeDadosException::class)
-            )]
-        )]
-    )
     @GetMapping
     fun buscarPedidos() = ResponseEntity.ok().body(pedidoAdapter.buscarPedidos())
 
-    @Operation(summary = "Cadastrar um pedido")
-    @ApiResponses(
-        value = [
-            ApiResponse(
-                responseCode = "201", description = "Created",
-                content = [(Content(
-                    mediaType = "application/json",
-                    schema = Schema(implementation = PedidoResponse::class)
-                ))]
-            ),
-            ApiResponse(
-                responseCode = "500", description = "Internal Server Error",
-                content = [Content(
-                    mediaType = "application/json",
-                    schema = Schema(implementation = BaseDeDadosException::class)
-                )]
-            )]
-    )
     @PostMapping
     fun post(@RequestBody request: CadastrarPedidoRequest): ResponseEntity<PedidoResponse> {
         val pedido = pedidoAdapter.cadastrarPedido(request)
         return ResponseEntity.created(URI.create("/pedidos/${pedido.id}")).body(pedido)
     }
 
-    @Operation(summary = "Alterar status de um pedido")
-    @ApiResponses(
-        value = [
-            ApiResponse(
-                responseCode = "200", description = "Ok",
-                content = [(Content(
-                    mediaType = "application/json"
-                ))]
-            ),
-            ApiResponse(
-                responseCode = "500", description = "Internal Server Error",
-                content = [Content(
-                    mediaType = "application/json",
-                    schema = Schema(implementation = BaseDeDadosException::class)
-                )]
-            )]
-    )
     @PatchMapping("/status")
     fun alterarStatusPedido(@RequestBody alterarStatusPedidoRequest: AlterarStatusPedidoRequest) =
         pedidoAdapter.alterarStatusPedido(alterarStatusPedidoRequest)
 
-    @Operation(summary = "Consultar status do pagamento de um pedido")
-    @ApiResponses(
-        value = [
-            ApiResponse(
-                responseCode = "200", description = "Ok",
-                content = [(Content(
-                    mediaType = "application/json",
-                    schema = Schema(implementation = PagamentoStatusResponse::class)
-                ))]
-            ),
-            ApiResponse(
-                responseCode = "500", description = "Internal Server Error",
-                content = [Content(
-                    mediaType = "application/json",
-                    schema = Schema(implementation = BaseDeDadosException::class)
-                )]
-            )]
-    )
     @GetMapping("/{id}/pagamento/status")
     fun consultarStatusPagamento(@PathVariable id: Long) = pedidoAdapter.consultarStatusPagamento(id)
 }
