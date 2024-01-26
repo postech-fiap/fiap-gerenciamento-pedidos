da865de6e2d0a74346736e66191f091fab34170d
Everton91Almeida <everton.almeida@mercadolivre.com>
2024-01-25 21:46:34 -0300

    fix payment payload

--- app/api/build.gradle.kts
+++ app/api/build.gradle.kts
@@ -2,6 +2,7 @@ dependencies {
     implementation(project(":app:domain"))
     implementation(project(":app:infrastructure"))
     implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.apache.httpcomponents.client5:httpclient5:5.3.1")
     developmentOnly("org.springframework.boot:spring-boot-devtools")
     testImplementation("io.mockk:mockk:1.13.5")
     testImplementation("com.h2database:h2:2.2.224")
--- app/api/src/test/kotlin/br/com/fiap/gerenciamentopedidos/api/controllers/PedidoControllerTest.kt
+++ app/api/src/test/kotlin/br/com/fiap/gerenciamentopedidos/api/controllers/PedidoControllerTest.kt
@@ -1,12 +1,17 @@
 package br.com.fiap.gerenciamentopedidos.api.controllers
 
 import br.com.fiap.gerenciamentopedidos.api.Application
+import br.com.fiap.gerenciamentopedidos.domain.enums.PagamentoStatus
+import org.json.JSONObject
+import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.web.client.TestRestTemplate
+import org.springframework.http.*
 import org.springframework.test.context.junit.jupiter.SpringExtension
+import java.util.*
 
 
 @ExtendWith(SpringExtension::class)
@@ -18,10 +23,26 @@ class PedidoControllerTest {
 
     @Test
     fun `deve cadastrar pedido com sucesso`() {
-
     }
 
     @Test
     fun `deve alterar status do pedido com sucesso`() {
+        val request = JSONObject()
+            .put("referencia", UUID.randomUUID())
+            .put("pagamento_id", 1L)
+            .put("status", PagamentoStatus.APROVADO)
+
+        val reqHeaders = HttpHeaders()
+        reqHeaders.contentType = MediaType.APPLICATION_JSON
+        reqHeaders.accept = listOf(MediaType.APPLICATION_JSON)
+
+        val responseEntity = testRestTemplate.exchange(
+            "/pedidos/status",
+            HttpMethod.PATCH,
+            HttpEntity(request.toString(), reqHeaders),
+            Any::class.java
+        )
+
+        Assertions.assertEquals(HttpStatus.NOT_FOUND, responseEntity.statusCode)
     }
 }
