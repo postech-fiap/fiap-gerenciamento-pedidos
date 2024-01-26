ca6e2c3b4993c57c2f046cf90134f0fed6eab4dd
Everton91Almeida <everton.almeida@mercadolivre.com>
2024-01-25 23:13:04 -0300

    fix payment payload

--- app/api/src/main/kotlin/br/com/fiap/gerenciamentopedidos/api/adapters/PedidoAdapterImpl.kt
+++ app/api/src/main/kotlin/br/com/fiap/gerenciamentopedidos/api/adapters/PedidoAdapterImpl.kt
@@ -4,8 +4,10 @@ import br.com.fiap.gerenciamentopedidos.api.adapters.interfaces.PedidoAdapter
 import br.com.fiap.gerenciamentopedidos.api.requests.AlterarStatusPedidoRequest
 import br.com.fiap.gerenciamentopedidos.api.requests.CadastrarPedidoRequest
 import br.com.fiap.gerenciamentopedidos.api.responses.PedidoResponse
+import br.com.fiap.gerenciamentopedidos.domain.enums.PagamentoStatus
 import br.com.fiap.gerenciamentopedidos.domain.interfaces.usecases.pedido.AlterarStatusPedidoUseCase
 import br.com.fiap.gerenciamentopedidos.domain.interfaces.usecases.pedido.CadastrarPedidoUseCase
+import java.util.*
 
 class PedidoAdapterImpl(
     private val cadastrarPedidoUseCase: CadastrarPedidoUseCase,
@@ -15,5 +17,8 @@ class PedidoAdapterImpl(
         PedidoResponse(cadastrarPedidoUseCase.executar(request.clienteId, request.produtos!!.map { it.toModel() }))
 
     override fun alterarStatusPedido(request: AlterarStatusPedidoRequest) =
-        alterarStatusPedidoUseCase.executar(request.referenciaPedido, request.statusPagamento)
+        alterarStatusPedidoUseCase.executar(
+            UUID.fromString(request.referenciaPedido),
+            PagamentoStatus.valueOf(request.statusPagamento)
+        )
 }
--- app/api/src/main/kotlin/br/com/fiap/gerenciamentopedidos/api/requests/AlterarStatusPedidoRequest.kt
+++ app/api/src/main/kotlin/br/com/fiap/gerenciamentopedidos/api/requests/AlterarStatusPedidoRequest.kt
@@ -1,10 +1,14 @@
 package br.com.fiap.gerenciamentopedidos.api.requests
 
 import br.com.fiap.gerenciamentopedidos.domain.enums.PagamentoStatus
+import com.fasterxml.jackson.annotation.JsonProperty
 import java.util.*
 
 data class AlterarStatusPedidoRequest(
-    val referenciaPedido: UUID,
+    @JsonProperty("referencia_pedido")
+    val referenciaPedido: String,
+    @JsonProperty("id_pagamento")
     val idPagamento: Long,
-    val statusPagamento: PagamentoStatus,
+    @JsonProperty("status_pagamento")
+    val statusPagamento: String,
 )
