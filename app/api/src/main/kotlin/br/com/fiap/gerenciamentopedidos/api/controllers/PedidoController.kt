1995f6d0a6e7e80a49ca4c837e1c936768b631e0
Everton91Almeida <everton.almeida@mercadolivre.com>
2024-03-12 22:26:29 -0300

    fix tests

--- app/api/src/main/kotlin/br/com/fiap/gerenciamentopedidos/api/controllers/PedidoController.kt
+++ app/api/src/main/kotlin/br/com/fiap/gerenciamentopedidos/api/controllers/PedidoController.kt
@@ -3,19 +3,21 @@ package br.com.fiap.gerenciamentopedidos.api.controllers
 import br.com.fiap.gerenciamentopedidos.api.adapters.interfaces.PedidoAdapter
 import br.com.fiap.gerenciamentopedidos.api.requests.CadastrarPedidoRequest
 import br.com.fiap.gerenciamentopedidos.api.responses.PedidoResponse
+import br.com.fiap.gerenciamentopedidos.domain.interfaces.PedidoRepository
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 import java.net.URI
 
 @RestController
 @RequestMapping("/pedidos")
-class PedidoController(private val pedidoAdapter: PedidoAdapter) {
+class PedidoController(private val pedidoAdapter: PedidoAdapter, private val repo: PedidoRepository) {
     @PostMapping
     fun post(@RequestBody request: CadastrarPedidoRequest): ResponseEntity<PedidoResponse> {
         val pedido = pedidoAdapter.cadastrarPedido(request)
         return ResponseEntity.created(URI.create("/pedidos/${pedido.id}")).body(pedido)
     }
+
+    //TODO: APAGAR
+    @GetMapping("/{id}")
+    fun get(@PathVariable id: Long) = ResponseEntity.ok(repo.buscarPedidoPorId(id).get())
 }
